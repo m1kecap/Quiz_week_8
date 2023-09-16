835b5bbde18901fe959de2bc95354eea7d40b0d3
m1kecap <nhatanhcap@gmail.com>
2023-09-16 19:13:12 +1000

    Code was added to load data from CSV

--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+data = pd.read_csv('climate.csv')
+
+years = data['Year']
+co2 = data['CO2']
+temp = data['Temperature']
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
