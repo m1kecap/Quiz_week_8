3388bcea6d3df659c43f6d22198089b139496a52
m1kecap <nhatanhcap@gmail.com>
2023-09-16 18:45:26 +1000

    Code was added to fetch data from database

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3        
+con = sqlite3.connect('climate.db')
+curs = con.cursor()
 years = []
 co2 = []
 temp = []
+
+for row in curs.execute('SELECT Year, CO2, Temperature FROM ClimateData'):
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
+con.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
